492130abb84cb499aea2a73cebdacd5cc3707a63
dorshaul1 <dor.asti@gmail.com>
2021-02-17 16:36:51 +0200

    adding Curr position function

--- js/map-controller.js
+++ js/map-controller.js
@@ -1,44 +1,48 @@
-import { mapService } from './services/map-service.js'
+import {
+    mapService
+} from './services/map-service.js'
 
 var gMap;
-console.log('Main!');
+// console.log('Main!');
 
 
 mapService.getLocs()
     .then(locs => console.log('locs', locs))
 
 window.onload = () => {
-    
-    document.querySelector('.btn').addEventListener('click', (ev) => {
-        console.log('Aha!', ev.target);
-        panTo(35.6895, 139.6917);
-    })
-
     initMap()
         .then(() => {
-            addMarker({ lat: 32.0749831, lng: 34.9120554 });
+            addMarker({
+                lat: 32.0749831,
+                lng: 34.9120554
+            });
         })
         .catch(() => console.log('INIT MAP ERROR'));
 
     getPosition()
         .then(pos => {
             console.log('User position is:', pos.coords);
+            // console.log('pos.coords:', pos.coords)
         })
         .catch(err => {
             console.log('err!!!', err);
         })
+    addEventsListeners()
 }
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
-    console.log('InitMap');
+    // console.log('InitMap');
     return _connectGoogleApi()
         .then(() => {
             console.log('google available');
             gMap = new google.maps.Map(
                 document.querySelector('#map'), {
-                center: { lat, lng },
-                zoom: 15
-            })
+                    center: {
+                        lat,
+                        lng
+                    },
+                    zoom: 15
+                })
             console.log('Map!', gMap);
         })
 }
@@ -59,16 +63,15 @@ function panTo(lat, lng) {
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-    console.log('Getting Pos');
+    // console.log('Getting Pos');
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
     })
 }
 
-
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyBVQipjJ0ddfwLp8ooqI_wUJEjIogAff5g'; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyBVQipjJ0ddfwLp8ooqI_wUJEjIogAff5g';
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
@@ -80,5 +83,43 @@ function _connectGoogleApi() {
     })
 }
 
+function addEventsListeners() {
+    onGetCurrPosition()
+    onClickMap()
+}
 
+function onGetCurrPosition() {
+    document.querySelector('.my-location-btn').addEventListener('click', (ev) => {
+        // console.log('Aha!', ev.target);
+        // panTo(35.6895, 139.6917);
+        getPosition()
+            .then(pos => {
+                panTo(pos.coords.latitude, pos.coords.longitude);
 
+                // console.log('pos.coords:', pos.coords)
+            })
+    })
+}
+
+function onClickMap() {
+        // let infoWindow = new google.maps.InfoWindow({
+        //     content: "Click the map to get Lat/Lng!",
+        //     position: myLatlng,
+        // });
+        // infoWindow.open(map);
+    // Configure the click listener.
+    console.log('gMap:', gMap)
+    gMap.addListener("click", (mapsMouseEvent) => {
+        // Close the current InfoWindow.
+            // infoWindow.close();
+        // Create a new InfoWindow.
+            //infoWindow = new google.maps.InfoWindow({
+            position: 
+            console.log(mapsMouseEvent.latLng)
+        });
+    //     infoWindow.setContent(
+    //         JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+    //     );
+    //     infoWindow.open(map);
+    // });
+}
